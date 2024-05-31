c55ec07f7e447ee4b6f71e35333d9c50dda300be
Minju9187 <alswn1583@naver.com>
2024-05-31 15:53:43 +0900

    firebase 연동 & 모든 할일 가져오기

---
+++ app/api/todos/[slug]/route.ts
@@ -0,0 +1,66 @@
+import { NextRequest, NextResponse } from "next/server";
+
+//할일 단일 조회
+export async function GET(
+  request: NextRequest,
+  { params }: { params: { slug: string } }
+) {
+  const searchParams = request.nextUrl.searchParams;
+
+  //   const search = searchParams.get("search");
+  const query = searchParams.get("query");
+
+  // URL -> `/dashboard?search=my-project`
+  // `search` -> 'my-project'
+
+  const response = {
+    message: "단일 할일 가져오기 성공",
+    data: {
+      id: params.slug,
+      title: "오늘도 빡코딩!",
+      is_done: false,
+      //   query: search,
+      query,
+    },
+  };
+
+  return NextResponse.json(response, { status: 200 });
+}
+
+// 할일 단일 삭제 id
+export async function DELETE(
+  request: NextRequest,
+  { params }: { params: { slug: string } }
+) {
+  const response = {
+    message: "단일 할일 삭제 성공",
+    data: {
+      id: params.slug,
+      title: "오늘도 빡코딩!",
+      is_done: false,
+    },
+  };
+
+  return NextResponse.json(response, { status: 200 });
+}
+
+// 할일 단일 수정 id
+export async function POST(
+  request: NextRequest,
+  { params }: { params: { slug: string } }
+) {
+  const { title, is_done } = await request.json();
+
+  const editedTodo = {
+    id: params.slug,
+    title,
+    is_done,
+  };
+
+  const response = {
+    message: "단일 할일 수정 성공",
+    data: editedTodo,
+  };
+
+  return NextResponse.json(response, { status: 200 });
+}
---
+++ data/firestore.js
@@ -0,0 +1,44 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, getDocs } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+//모든 할일 가져오기
+export async function fetchTodos() {
+  const querySnapshot = await getDocs(collection(db, "todos"));
+
+  if (querySnapshot.empty) {
+    return [];
+  }
+
+  const fetchedTodos = [];
+
+  querySnapshot.forEach((doc) => {
+    // doc.data() is never undefined for query doc snapshots
+    console.log(doc.id, " => ", doc.data());
+
+    const aTodo = {
+      id: doc.id,
+      title: doc.data()["title"],
+      is_done: doc.data()["is_done"],
+      created_at: doc.data()["created_at"],
+    };
+    fetchedTodos.push(aTodo);
+  });
+  return fetchedTodos;
+}
